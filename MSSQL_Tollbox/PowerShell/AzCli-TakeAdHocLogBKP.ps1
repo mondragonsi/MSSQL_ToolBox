7b8bb0357d1e92ab6e6d83d849e00bbd38f9a7b1
Bruno Vieira Mondragon <mondragon.si@gmail.com>
2023-04-22 02:03:56 -0300

    First files
    starting journey

---
+++ MSSQL_Tollbox/PowerShell/AzCli-TakeAdHocLogBKP.ps1
@@ -0,0 +1,29 @@
+﻿$dbName          = "db_name"
+$ItemName        = "SQLDataBase;AGPRD;" #Name of your AG. You can find it in the azure portal JSON option
+$AGNameContainer = "SQLAGWorkLoadContainer;074d1466-xxxx-xxxx-xxxx-xxxxxxxxxc" 
+$finalName       = $ItemName + $dbName
+
+Write-Host $finalName
+
+
+#Starts a backup ad-hoc type LOG on Azure Service Vault
+az backup protection backup-now `
+--resource-group rg-myLIMS-prd `
+--item-name $finalName `
+--vault-name recover-prd `
+--container-name $AGNameContainer `
+--backup-typ Log `
+--workload-type MSSQL `
+--backup-management-type AzureWorkload `
+--output table `
+| Out-GridView
+
+#Gets a list of backup Jobs in Progress
+az backup job list `
+--resource-group rg-myLIMS-prd `
+--vault-name recover-prd `
+--backup-management-type AzureWorkload `
+--operation Backup `
+--status InProgress  `
+--output table  `
+| Out-GridView                                                           
---
+++ MSSQL_Tollbox/StoredProcedures/Maint_IX_GetListOfDB_NAMES_BY_Size.sql
@@ -0,0 +1,80 @@
+USE [master]
+GO
+
+/****** Object:  StoredProcedure [dbo].[Maint_IX_GetListOfDB_NAMES_BY_Size]    Script Date: 22/04/2023 01:56:54 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dbo].[Maint_IX_GetListOfDB_NAMES_BY_Size] 
+    @SizeGB_Low_PARM INT, 
+    @SizeGB_High_PARM INT, 
+    @Result VARCHAR(MAX) OUTPUT
+AS
+BEGIN
+
+DECLARE @SizeGB_Low    INT = @SizeGB_Low_PARM;
+DECLARE @SizeGB_High   INT = @SizeGB_High_PARM; 
+
+DROP TABLE IF EXISTS #DBListNameSize;
+
+--CREATES A TEMPORARY TABLE
+CREATE TABLE #DBListNameSize
+(Id INT IDENTITY, name NVARCHAR(100), sizeInTemp decimal(10,2));
+
+--QUERY ALL USER DATABASES AND THEIR SIZE
+WITH ListDBS_CTE
+AS (
+   SELECT d.NAME
+    ,(SUM(CAST(mf.size AS decimal(10,2))) * 8.0 / 1024) / 1024 AS Size_GBs
+FROM sys.master_files mf
+INNER JOIN sys.databases d ON d.database_id = mf.database_id
+WHERE d.database_id > 4 -- Skip system databases
+GROUP BY d.NAME)
+
+
+INSERT INTO #DBListNameSize
+SELECT *
+FROM ListDBS_CTE
+WHERE Size_GBs BETWEEN @SizeGB_Low AND @SizeGB_High;          ----DEFINE RANGE OF DBS by SIZE
+
+DECLARE @Counter INT;
+DECLARE @MaxId   INT; 
+DECLARE @DBName NVARCHAR(100);
+DECLARE @Size             DECIMAL(10,2) = 0.00;
+DECLARE @SizeAcumulated   DECIMAL(10,2) = 0.00;
+
+SELECT @Counter = min(Id), 
+       @MaxId   = max(Id) 
+FROM #DBListNameSize;
+
+WHILE(@Counter IS NOT NULL
+      AND @Counter <= @MaxId)
+BEGIN
+   SELECT 
+   @DBName = name,
+   @Size = sizeInTemp  
+   FROM #DBListNameSize
+   WHERE Id = @Counter;
+
+   SET @SizeAcumulated = @SizeAcumulated + @Size;
+   SET @Result = @Result + @DBName + ',';
+    
+   PRINT 'Concataning: ' + @DBName + '------- SIZE: ' + CAST(@Size AS NVARCHAR(50)) + ' ACUMMULATED: ' + CAST(@SizeAcumulated AS NVARCHAR(50)) ;
+   SET @Counter  = @Counter  + 1;      
+END
+
+IF @Result <> ''
+    SET @Result = LEFT(@Result, LEN(@Result) - 1);
+ELSE
+    SET @Result = 'NO DATABASES WERE RETURNED. NOTHING TO PROCESS!';
+
+--SELECT @Result AS MyList;
+--RETURN @Result;
+
+END
+GO
+
